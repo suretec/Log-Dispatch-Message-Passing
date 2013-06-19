7488d6e77f51c6c30b694f7f6aa82249620a4292
Chris Prather <chris@prather.org>
2013-06-19 18:55:04 -0400

    Revert "Work with upcoming versions of Log::Dispatch"
    This reverts commit d854a231b93ea56312050375aa641fcf81f37e04.
    
    Conflicts:
    	Changes

--- lib/Log/Dispatch/Message/Passing.pm
+++ lib/Log/Dispatch/Message/Passing.pm
@@ -27,10 +27,6 @@ sub new {
 
 sub log_message {
   my ($self, %p) = @_;
-  if ($p{level} =~ /^\d+$/) {
-      require Log::Dispatch::Util;
-      $p{level} = $Log::Dispatch::Util::LevelNames[ $p{level} ];
-  }
   $self->{output}->consume({%p});
 }
 
